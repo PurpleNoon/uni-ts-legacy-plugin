8f46875f2a15202bde12f2f9479d5c35ffed7818
zhangzemeng <m13848247612@163.com>
2024-05-27 17:39:28 +0800

    feat: 添加 @vitejs/plugin-legacy，但微信小程序无法构建

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [
+    uni(),
+    legacy(),
+  ],
 });
